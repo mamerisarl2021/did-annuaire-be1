39a3740b5a3da435858cfa4c754c67090243d202
davieddee <princedavealex.20@gmail.com>
2026-01-22 15:43:01 +0100

    update(settings, urls): bring back orbit with RECORD_COMMAND disabled add(settings): add cors to settings update(uv.lock): update dependencies

--- config/settings/cors.py
+++ config/settings/cors.py
@@ -1,2 +1,9 @@
+from config.env import env
+
 CORS_URLS_REGEX = r"^/api/.*$"
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = []
+ENV_CORS_ALLOWED_ORIGINS = env.str("CORS_ALLOWED_ORIGINS", default="")
+for origin in ENV_CORS_ALLOWED_ORIGINS.split(","):
+    CORS_ALLOWED_ORIGINS.append(f"{origin}".strip().lower())
