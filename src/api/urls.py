8b9f0cc5d4f371dcd4d134ca4b069295cb318bb3
davieddee <princedavealex.20@gmail.com>
2026-01-23 12:46:12 +0100

    fix: disable orbit in prod

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,21 +2,21 @@ from ninja_extra import NinjaExtraAPI
 from ninja_jwt.controller import NinjaJWTDefaultController
 
 from src.api.exception_handler import attach_exception_handlers
-from src.dids.api_certificates import CertificateController
-from src.dids.api_registry_create import DIDCreationController
-from src.dids.apis_registry import RegistryController
+
+from src.dids.did_registry_api.controllers.certificates_controller import CertificatesController
+from src.dids.did_registry_api.controllers.keys_controller import KeysController
+from src.dids.did_registry_api.controllers.publish_requests_controller import PublishRequestsController
+from src.dids.did_registry_api.controllers.registry_controller import RegistryController
+from src.dids.did_registry_api.controllers.universal_registrar_controller import UniversalRegistrarController
+from src.dids.resolver.controllers import ResolverController
 
 from src.organizations.apis import OrganizationController
 from src.users.apis import UserController
-from src.auth.controllers.logout_controller import LogoutController
-from src.dids.apis_universal import (
-    UniversalRegistrarController,
-    UniversalResolverController,
-)
-from src.dids.apis_version_service import VersionServiceController
-from src.api_keys.apis import APIKeyController
+# from src.api_keys.apis import APIKeyController
 from src.auditaction.apis import AuditActionController
 from src.superadmin.apis import SuperAdminController
+from src.auth.controllers.logout_controller import LogoutController
+
 
 api = NinjaExtraAPI(title="DID Annuaire API", version="1.0.0", csrf=False)
 
@@ -30,13 +30,13 @@ api.register_controllers(
     SuperAdminController,
     OrganizationController,
     UserController,
-    LogoutController,
-    APIKeyController,
+    # APIKeyController,
     AuditActionController,
-    UniversalRegistrarController,
-    UniversalResolverController,
-    VersionServiceController,
     RegistryController,
-    CertificateController,
-    DIDCreationController,
+    KeysController,
+    PublishRequestsController,
+    UniversalRegistrarController,
+    CertificatesController,
+    ResolverController,
+    LogoutController,
 )
